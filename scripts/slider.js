8f390a03a9c9ab51462c2df01e6b89599dd08cfd
pavkrol <pdkrol@gmail.com>
2019-06-19 15:51:01 +0200

    slider for mobile done, desktop version in progress

--- scripts/slider.js
+++ scripts/slider.js
@@ -1,4 +1,8 @@
 const slider = document.getElementById("slider");
+const bttn_left = document.getElementById("slider__bttn--left");
+const bttn_right = document.getElementById("slider__bttn--right");
+const items = document.querySelectorAll(".slider__item");
+
 let current_slide = 0,
   swipe_start,
   swipe_end;
@@ -16,6 +20,23 @@ slider.addEventListener("mouseup", e => {
     current_slide++;
   } else if (swipe < -20) {
     current_slide--;
+  }
+});
+
+bttn_left.addEventListener("click", () => {
+  if (current_slide > 0) {
+    items[current_slide].classList.toggle("slider__item--right");
+    current_slide--;
+    items[current_slide].classList.toggle("slider__item--left");
+  }
+  console.log(current_slide);
+});
+
+bttn_right.addEventListener("click", () => {
+  if (current_slide < items.length - 1) {
+    items[current_slide].classList.toggle("slider__item--left");
+    current_slide++;
+    items[current_slide].classList.toggle("slider__item--right");
   }
   console.log(current_slide);
 });
