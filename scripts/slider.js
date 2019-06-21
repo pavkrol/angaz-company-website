d2ae643442599693377e5f8e9ea60b85128b8f5f
pavkrol <pdkrol@gmail.com>
2019-06-21 14:00:26 +0200

    slider section completed, some bugs with font size fixed

--- scripts/slider.js
+++ scripts/slider.js
@@ -2,6 +2,7 @@ const slider = document.getElementById("slider");
 const bttn_left = document.getElementById("slider__bttn--left");
 const bttn_right = document.getElementById("slider__bttn--right");
 const items = document.querySelectorAll(".slider__item");
+const dots = document.querySelectorAll(".dots__item");
 
 let current_slide = 0,
   swipe_start,
@@ -26,17 +27,19 @@ slider.addEventListener("mouseup", e => {
 bttn_left.addEventListener("click", () => {
   if (current_slide > 0) {
     items[current_slide].classList.toggle("slider__item--right");
+    dots[current_slide].classList.toggle("dots__item--active");
     current_slide--;
     items[current_slide].classList.toggle("slider__item--left");
+    dots[current_slide].classList.toggle("dots__item--active");
   }
-  console.log(current_slide);
 });
 
 bttn_right.addEventListener("click", () => {
   if (current_slide < items.length - 1) {
     items[current_slide].classList.toggle("slider__item--left");
+    dots[current_slide].classList.toggle("dots__item--active");
     current_slide++;
     items[current_slide].classList.toggle("slider__item--right");
+    dots[current_slide].classList.toggle("dots__item--active");
   }
-  console.log(current_slide);
 });
